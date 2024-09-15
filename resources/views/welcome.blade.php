89bf7e2042538425ec0e800772731ad0da968580
Matt Nguyen <newyenmatt@gmail.com>
2024-09-15 11:24:56 -0500

    Fav Icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- For Login stuff. May need to delete if not needed? -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
 
-
-        <title>Laravel</title>
+        <title>Peu Research Center</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
