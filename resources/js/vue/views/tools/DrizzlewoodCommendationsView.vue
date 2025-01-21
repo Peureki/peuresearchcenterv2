487403d42ec4e9a36857e1df1527eb97f8b36214
Matt Nguyen <newyenmatt@gmail.com>
2025-01-21 17:34:15 -0600

    Reward track details

--- resources/js/vue/views/tools/DrizzlewoodCommendationsView.vue
+++ resources/js/vue/views/tools/DrizzlewoodCommendationsView.vue
@@ -1,6 +1,22 @@
 <template>
     <Header page-name="Drizzlewood Commendations"/>
-    <Nav/>
+    <Nav>
+        <!--
+            *
+            * API REFRESH PANEL
+            *
+        -->
+        <!-- <template v-slot:countdown v-if="user">
+            <div class="countdown-container" v-if="user.has_api_key">
+                <div class="countdown">
+                    <p>API Refresh</p>
+                    <p>{{ formatTime(refreshCountdown) }}</p>
+                </div>
+
+                <p class="small-subtitle">Please allow 5-10 mins to update your achievements</p>                
+            </div>
+        </template> -->
+    </Nav>
     <!-- <CurrencyPage
         page-name="Ash Legion Commendation"
         target-currency="Ash Legion Commendation"
@@ -60,32 +76,61 @@
             </div> 
 
             <div v-if="repeatableRewardTracks" v-for="(achievement, index) in repeatableRewardTracks" :key="index" class="reward-track-container">
-
+                <!--
+                    *
+                    * REWARD TRACK HEADER
+                    *
+                -->
                 <div class="reward-track-header">
-                    <!-- ACHIEVEMENT NAME -->
-                    <p class="title">{{ commendationID(achievement.id).name }}</p>
+                    <div class="flex">
+                        <!-- ACHIEVEMENT NAME -->
+                        <p class="title">{{ commendationID(achievement.id).name }}</p>
+
+                        <svg 
+                            class="arrow"
+                            @click="expand[index] = !expand[index]"
+                            :class="activeArrow(expand[index])"
+                            width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M0.32246 8.33324V6.66657L10.3225 6.66657L5.73913 2.08324L6.92246 0.899902L13.5225 7.4999L6.92246 14.0999L5.73913 12.9166L10.3225 8.33324H0.32246Z" fill="#FFFFFF"/>
+                        </svg>
+                    </div>
+                    
                     
                     <!-- TOTAL REWARD TRACK VALUE -->
-                    <span class="gold-label-container">
-                        <span 
-                            class="gold-label"
-                            v-for="gold in formatValue(totalRewardTrackValue(sortedCommendations[index]))"
-                        >
-                            <p>{{ gold.value }}</p><img :src="gold.src" :alt="gold.alt" :title="gold.title">
+                    <div class="flex">
+                        <p class="small-subtitle">Remaining: </p>
+                        <span class="gold-label-container">
+                            <span 
+                                class="gold-label"
+                                v-for="gold in formatValue(rewardTrackValueLeft(achievement, sortedCommendations[index]))"
+                            >
+                                <p>{{ gold.value }}</p><img :src="gold.src" :alt="gold.alt" :title="gold.title">
+                            </span>
                         </span>
-                    </span>
+                    </div>
+                    
+                </div>
+
+                <div class="next-reward-container">
+                    <!-- <p>Next reward: </p> -->
+
+                    <!-- <img class="card-main-icon" :src="nextReward(achievement, sortedCommendations[index]).icon" :alt="nextReward(achievement, sortedCommendations[index]).name" :title="nextReward(achievement, sortedCommendations[index]).name"> -->
+
+                    <!-- <p>{{ nextReward(achievement, sortedCommendations[index]).name }}</p> -->
+                    
                 </div>
                 
 
-                <div class="reward-track-bar-container">
+                <div class="bar-container">
                     <!--
                         *
                         * REWARD TRACK BAR
                         *
                     -->
-                    <div class="reward-track-bar">
+                    <div class="bar">
                         <!-- CURRENT REWARD TRACK PROGRESS -->
-                        <div class="reward-track-progress-container">
+                        <div class="progress-container">
                             <p class="reward-track-percentage small-subtitle">{{ getProgressionPercentage(achievement.current, achievement.max) }}</p>
 
                             <p class="small-subtitle">{{ getProgressionAmount(achievement) }}</p>          
@@ -127,7 +172,7 @@
                                 left: `${n * 5}%`
                                 }"
                         >
-                            <img class="icon" :src="getRewardTrackItem(commendations[index][subIndex]).icon" :alt="getRewardTrackItem(commendations[index][subIndex]).name" :title="getRewardTrackItem(commendations[index][subIndex]).name">
+                            <img class="icon" :style="{opacity: opacityRewardsAchieved(achievement, subIndex)}" :src="getRewardTrackItem(sortedCommendations[index][subIndex]).icon" :alt="getRewardTrackItem(sortedCommendations[index][subIndex]).name" :title="getRewardTrackItem(sortedCommendations[index][subIndex]).name">
                         </span>
                     </div>
                     <!--
@@ -136,34 +181,68 @@
                         *
                     -->
                     <div class="final-reward">
-                        <img class="card-main-icon" :src="getRewardTrackItem(commendations[index][19]).icon">
+                        <img class="card-main-icon" :src="getRewardTrackItem(sortedCommendations[index][19]).icon">
+                    </div>
+                </div>
+
+                <!--
+                    *
+                    * REWARD ITEM DETAILS
+                    *
+                -->
+                <div v-if="expand[index]" class="mobile-benchmark-details-container">
+                    <div v-for="(n, subIndex) in 20" class="mobile-drops-container">
+                        <div class="label-and-cta">
+                            <!-- ICON, NAME -->
+                            <div class="img-and-label">
+                                <img :src="getRewardTrackItem(sortedCommendations[index][subIndex]).icon" :alt="getRewardTrackItem(sortedCommendations[index][subIndex]).name" :title="getRewardTrackItem(sortedCommendations[index][subIndex]).name">
+                                <p>{{ getRewardTrackItem(sortedCommendations[index][subIndex]).name }}</p>
+                            </div>
+                            <!-- WIKI -->
+                            <svg class="icon clickable" @click="wiki(getRewardTrackItem(sortedCommendations[index][subIndex]).name)" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14 0C11.2311 0 8.52431 0.821086 6.22202 2.35943C3.91973 3.89777 2.12532 6.08427 1.06569 8.64243C0.00606596 11.2006 -0.271181 14.0155 0.269012 16.7313C0.809205 19.447 2.14258 21.9416 4.10051 23.8995C6.05845 25.8574 8.55301 27.1908 11.2687 27.731C13.9845 28.2712 16.7994 27.9939 19.3576 26.9343C21.9157 25.8747 24.1022 24.0803 25.6406 21.778C27.1789 19.4757 28 16.7689 28 14C28 10.287 26.525 6.72601 23.8995 4.1005C21.274 1.475 17.713 0 14 0ZM26 13H20C19.8833 9.31709 18.9291 5.70915 17.21 2.45C19.5786 3.0979 21.6914 4.45684 23.2632 6.34342C24.8351 8.23 25.7903 10.5534 26 13ZM14 26C13.7769 26.015 13.5531 26.015 13.33 26C11.2583 22.6962 10.1085 18.8981 10 15H18C17.9005 18.8953 16.7612 22.6932 14.7 26C14.467 26.0164 14.2331 26.0164 14 26ZM10 13C10.0995 9.10468 11.2388 5.30682 13.3 2C13.7453 1.94997 14.1947 1.94997 14.64 2C16.7223 5.3008 17.8825 9.09906 18 13H10ZM10.76 2.45C9.0513 5.71164 8.10746 9.31945 8.00001 13H2.00001C2.20971 10.5534 3.16495 8.23 4.7368 6.34342C6.30865 4.45684 8.42144 3.0979 10.79 2.45H10.76ZM2.05001 15H8.05001C8.15437 18.6798 9.09478 22.2875 10.8 25.55C8.43887 24.8951 6.33478 23.5332 4.77056 21.6472C3.20634 19.7612 2.25695 17.4415 2.05001 15ZM17.21 25.55C18.9291 22.2908 19.8833 18.6829 20 15H26C25.7903 17.4466 24.8351 19.77 23.2632 21.6566C21.6914 23.5432 19.5786 24.9021 17.21 25.55Z" fill="var(--color-link)"/>
+                                <title>Wiki</title>
+                            </svg>
+                        </div>
+                        <!-- TIER POSITION -->
+                        <p class="small-subtitle">Tier: {{ subIndex + 1 }}</p>
+                        <!-- VALUE -->
+                        <span class="gold-label-container">
+                            <span 
+                                class="gold-label"
+                                v-for="gold in formatValue(getRewardTrackItem(sortedCommendations[index][subIndex]).value)"
+                            >
+                                <p>{{ gold.value }}</p><img :src="gold.src" :alt="gold.alt" :title="gold.title">
+                            </span>
+                        </span>
                     </div>
                 </div>
             </div>
 
         </div>
     </section>
+
+    <Loading v-if="!commendations || !repeatableRewardTracks" :width="200" :height="200"/>
 </template>
 
 <script setup>
 import Header from '@/js/vue/components/general/Header.vue'
 import Nav from '@/js/vue/components/general/Nav.vue'
-import CurrencyPage from '@/js/vue/components/general/CurrencyPage.vue'
-
-import Currency from '@/imgs/icons/Ash_Legion_Commendation.png'
+import Loading from '@/js/vue/components/general/Loading.vue'
 
 import { onMounted, ref } from 'vue';
 import { getAuthUser } from '@/js/vue/composables/Authentication';
 import { user, includes, buyOrder, sellOrder, tax } from '@/js/vue/composables/Global';
 import { formatValue, formatPercentage } from '@/js/vue/composables/FormatFunctions.js'
+import { wiki, activeArrow } from '@/js/vue/composables/BasicFunctions'
 import axios from 'axios';
-
-import Choya from '@/imgs/icons/Choya_Mining_Tool.png'
 
 const commendations = ref(null),
     sortedCommendations = ref(null); 
 
 const repeatableRewardTracks = ref(null);
+
+const expand = ref(null);
 
 onMounted(async () => {
     getAllCommendations(); 
@@ -198,6 +277,8 @@ const getUserRewardTrackProgress = async () => {
             });
             // console.log('track ids: ', trackIDs);
             // console.log('commendations map: ', sortedCommendations.value, commendations.value); 
+
+            expand.value = repeatableRewardTracks.value.map(() => false);
         }
     } catch (error){
         console.log("Could not get user's dwc achievements: ", error); 
@@ -278,14 +359,28 @@ const getRewardTrackItem = (reward) => {
     if (!reward.hasOwnProperty('0')){
         return {
             icon: reward.icon,
-            name: reward.name
+            name: reward.name,
+            value: reward.value,
         } 
     } else {
         return {
             icon: reward[0].bag_icon,
             name: reward[0].bag_name,
+            value: reward.value,
         } 
     }
+}
+
+// *
+// * GET NEXT REAWRD
+// * 
+// * 1) Calc progress in the achievement (.current, .max)
+// * 2) Each reward is 250 points out of 5000 (20 rewards total). Take the current progress, / 250, return respective reward in the track
+// *
+const nextReward = (achievement, track) => {
+    const currentProgress = Math.floor(achievement.current / 250); 
+    //console.log('next reward: ', track[currentProgress])
+    return getRewardTrackItem(track[currentProgress]); 
 }
 
 const getProgressionPercentage = (min, max) => {
@@ -294,6 +389,29 @@ const getProgressionPercentage = (min, max) => {
 
 const getProgressionAmount = (achievement) => {
     return `${achievement.current} / ${achievement.max}`
+}
+
+const opacityRewardsAchieved = (achievement, rewardIndex) => {
+    const currentProgressIndex = Math.floor(achievement.current / 250); 
+    return rewardIndex >= currentProgressIndex ? 1 : 0.3; 
+}
+
+const rewardTrackValueLeft = (achievement, track) => {
+    // Object.keys since commendations are Objects with both numerial properties and named properties
+    let value = 0;
+
+    const currentProgressIndex = Math.floor(achievement.current / 250); 
+
+    Object.keys(track).forEach((key, index) => {
+        // Check on numerical keys 
+        // If yes => continously add into value
+        if (index >= currentProgressIndex){
+            if (!isNaN(key)){
+                value += track[key].value; 
+            }
+        }
+    })
+    return value; 
 }
 
 // * 
@@ -309,7 +427,6 @@ const totalRewardTrackValue = (commendation) => {
             value += commendation[key].value; 
         }
     })
-
     return value; 
 }
 </script>
@@ -318,7 +435,7 @@ const totalRewardTrackValue = (commendation) => {
 .reward-track-container{
     padding-bottom: calc(var(--gap-content));
 }
-.reward-track-bar-container{
+.bar-container{
     display: flex;
     align-items: center;
     gap: var(--gap-content);
@@ -327,14 +444,14 @@ const totalRewardTrackValue = (commendation) => {
     display: flex;
     justify-content: space-between;
 }
-.reward-track-bar{
+.bar{
     position: relative; 
     width: 100%; 
     height: 30px;
     border: var(--border-general);
     border-radius: var(--border-radius-card);
 }
-.reward-track-bar > span.progress-separator{
+.bar > span.progress-separator{
     position: absolute;
     width: 2px;
     height: 100%;
@@ -342,7 +459,7 @@ const totalRewardTrackValue = (commendation) => {
     bottom: -50%;
     z-index: 3;
 }
-.reward-track-progress-container{
+.progress-container{
     position: absolute;
     display: flex;
     gap: var(--gap-content);
@@ -367,6 +484,14 @@ const totalRewardTrackValue = (commendation) => {
     z-index: 2;
     transition: var(--transition-all-03s-ease);
     border-radius: var(--border-radius-card);
+}
+.next-reward-container{
+    display: flex;
+    gap: var(--gap-general);
+}
+
+.mobile-benchmark-details-container{
+    padding-top: calc(var(--gap-content));
 }
 
 </style>
